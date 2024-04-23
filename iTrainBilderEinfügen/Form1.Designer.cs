2aeda079ad8000e384a97aaeac5c72d6fe005952
aveelsass <aveelsass@web.de>
2024-04-23 16:56:20 +0200

    Add project files.

---
+++ iTrainBilderEinfügen/Form1.Designer.cs
@@ -0,0 +1,158 @@
+﻿namespace iTrainBilderEinfügen
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if(disposing && (components != null)) {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.iTrainLayoutTB = new System.Windows.Forms.TextBox();
+            this.layoutBtn = new System.Windows.Forms.Button();
+            this.statusLbl = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.imageFldrTB = new System.Windows.Forms.TextBox();
+            this.ImgPathBtn = new System.Windows.Forms.Button();
+            this.StartBtn = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(31, 26);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(309, 195);
+            this.label1.TabIndex = 0;
+            this.label1.Text = resources.GetString("label1.Text");
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(31, 247);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(125, 13);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "iTrain Layout auswählen:";
+            // 
+            // iTrainLayoutTB
+            // 
+            this.iTrainLayoutTB.Location = new System.Drawing.Point(175, 244);
+            this.iTrainLayoutTB.Name = "iTrainLayoutTB";
+            this.iTrainLayoutTB.Size = new System.Drawing.Size(192, 20);
+            this.iTrainLayoutTB.TabIndex = 2;
+            this.iTrainLayoutTB.Text = "Basispfad\\layouts";
+            // 
+            // layoutBtn
+            // 
+            this.layoutBtn.Location = new System.Drawing.Point(373, 242);
+            this.layoutBtn.Name = "layoutBtn";
+            this.layoutBtn.Size = new System.Drawing.Size(47, 23);
+            this.layoutBtn.TabIndex = 3;
+            this.layoutBtn.Text = "...";
+            this.layoutBtn.UseVisualStyleBackColor = true;
+            this.layoutBtn.Click += new System.EventHandler(this.layoutBtn_Click);
+            // 
+            // statusLbl
+            // 
+            this.statusLbl.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)));
+            this.statusLbl.AutoSize = true;
+            this.statusLbl.Location = new System.Drawing.Point(3, 385);
+            this.statusLbl.Name = "statusLbl";
+            this.statusLbl.Size = new System.Drawing.Size(0, 13);
+            this.statusLbl.TabIndex = 4;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(31, 276);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(141, 13);
+            this.label3.TabIndex = 1;
+            this.label3.Text = "Ordner mit skalierten Bildern:";
+            // 
+            // imageFldrTB
+            // 
+            this.imageFldrTB.Location = new System.Drawing.Point(175, 273);
+            this.imageFldrTB.Name = "imageFldrTB";
+            this.imageFldrTB.Size = new System.Drawing.Size(192, 20);
+            this.imageFldrTB.TabIndex = 2;
+            this.imageFldrTB.Text = "Basispfad\\images";
+            // 
+            // ImgPathBtn
+            // 
+            this.ImgPathBtn.Location = new System.Drawing.Point(373, 271);
+            this.ImgPathBtn.Name = "ImgPathBtn";
+            this.ImgPathBtn.Size = new System.Drawing.Size(47, 23);
+            this.ImgPathBtn.TabIndex = 3;
+            this.ImgPathBtn.Text = "...";
+            this.ImgPathBtn.UseVisualStyleBackColor = true;
+            this.ImgPathBtn.Click += new System.EventHandler(this.ImgPathBtn_Click);
+            // 
+            // StartBtn
+            // 
+            this.StartBtn.Location = new System.Drawing.Point(34, 323);
+            this.StartBtn.Name = "StartBtn";
+            this.StartBtn.Size = new System.Drawing.Size(75, 23);
+            this.StartBtn.TabIndex = 5;
+            this.StartBtn.Text = "Start";
+            this.StartBtn.UseVisualStyleBackColor = true;
+            this.StartBtn.Click += new System.EventHandler(this.StartBtn_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 407);
+            this.Controls.Add(this.StartBtn);
+            this.Controls.Add(this.statusLbl);
+            this.Controls.Add(this.ImgPathBtn);
+            this.Controls.Add(this.imageFldrTB);
+            this.Controls.Add(this.layoutBtn);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.iTrainLayoutTB);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "iTrain Bilder einfügen";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox iTrainLayoutTB;
+        private System.Windows.Forms.Button layoutBtn;
+        private System.Windows.Forms.Label statusLbl;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox imageFldrTB;
+        private System.Windows.Forms.Button ImgPathBtn;
+        private System.Windows.Forms.Button StartBtn;
+    }
+}
+
---
+++ iTrainBilderEinfügen/Form1.cs
@@ -0,0 +1,214 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+using System.IO.Compression;
+using System.Xml;
+
+
+namespace iTrainBilderEinfügen
+{
+    public partial class Form1 : Form
+    {
+
+        #region Variables
+        private String basispfad = Environment.GetFolderPath(Environment.SpecialFolder.UserProfile) + "\\iTrain";
+        #endregion Variables
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        /// <summary>
+        ///  if the path begins with "Basispfad", replace that with basispfad and vice versa
+        /// </summary>
+        /// <param name="path"></param>
+        /// <param name="allowBack">Replace basispfad with "Basispfad" only if true and </param>
+        /// <returns></returns>
+        private String repBasispfad(String path, bool allowBack = true)
+        {
+            if(path.StartsWith("Basispfad")) {
+                return path.Replace("Basispfad", basispfad);
+            } else if (allowBack) {
+                return path.Replace(basispfad, "Basispfad");
+            } else {
+                return path;
+            }
+        }
+
+        private void layoutBtn_Click(Object sender, EventArgs e)
+        {
+            using(OpenFileDialog ofd = new OpenFileDialog()) {
+                ofd.InitialDirectory = repBasispfad(iTrainLayoutTB.Text);
+                ofd.Filter = "iTrain files (tcdz)|*.tcdz";
+                if(ofd.ShowDialog() == DialogResult.OK) {
+                    iTrainLayoutTB.Text = repBasispfad(ofd.FileName);
+                }
+            }
+        }
+
+        private void ImgPathBtn_Click(Object sender, EventArgs e)
+        {
+            using(FolderBrowserDialog fbd = new FolderBrowserDialog()) {
+                fbd.SelectedPath = repBasispfad(imageFldrTB.Text);
+                if(fbd.ShowDialog() == DialogResult.OK) {
+                    imageFldrTB.Text = repBasispfad(fbd.SelectedPath);
+                }
+            }
+        }
+
+        private void StartBtn_Click(Object sender, EventArgs e)
+        {
+            Cursor = Cursors.WaitCursor;
+            Status("");
+
+            // check layout-file and images folder for existence
+            String iTrainLayoutFile = repBasispfad(iTrainLayoutTB.Text, false);
+            String imageFolder = repBasispfad(imageFldrTB.Text, false);
+            if(!File.Exists(iTrainLayoutFile)) {
+                MessageBox.Show($"Layout '{iTrainLayoutFile}' konnte nicht gefunden werden!", "Datei nicht gefunden", MessageBoxButtons.OK, MessageBoxIcon.Exclamation);
+                Cursor = Cursors.Default;
+                return;
+            }
+            if(!Directory.Exists(imageFolder)) {
+                MessageBox.Show($"Bild-Ordner '{imageFolder}' konnte nicht gefunden werden!", "Ordner nicht gefunden", MessageBoxButtons.OK, MessageBoxIcon.Exclamation);
+                Cursor = Cursors.Default;
+                return;
+            }
+
+
+            Cursor = Cursors.Default;
+        }
+
+        /// <summary>
+        /// Update the status-bar
+        /// </summary>
+        /// <param name="msg"></param>
+        private void Status(String msg)
+        {
+            statusLbl.Text = msg;
+            Application.DoEvents(); 
+        }
+
+        #region iTrain access
+        /// <summary>
+        ///  open the zip-file (tcdz-file) with the given path 
+        ///  - save it to the global varialbe archive, 
+        ///  - open the xml-document 
+        ///  - read the image index
+        ///  - fill the listbox with its items
+        /// </summary>
+        /// <param name="zipPath"></param>
+        private XmlDocument openTCDZ(String archivePath)
+        {
+            ZipArchive archive = ZipFile.OpenRead(archivePath);
+            XmlDocument xmlDoc = null;
+            foreach(ZipArchiveEntry entry in archive.Entries) {
+                // load the tcd-xml file into a document.
+                if(entry.FullName.ToLower().EndsWith(".tcd")) {
+                    ZipArchiveEntry tcdEntry = entry;
+                    xmlDoc = new XmlDocument();
+                    xmlDoc.PreserveWhitespace = true;
+                    using(Stream fstrm = entry.Open()) {
+                        xmlDoc.Load(fstrm);
+                    }
+                    break;
+                }
+            }
+            return xmlDoc;
+        }
+
+        String getAttr(XmlNode node, String attrName, String defval = "-")
+        {
+            String res = node?.Attributes?[attrName]?.InnerText;
+            if(res == null) {
+                return defval;
+            }
+            return res;
+        }
+
+        String getSubAttr(XmlNode node, String subNode, String attrName, String defval = "-")
+        {
+            XmlNode sub = node?[subNode];
+            return getAttr(sub, attrName, defval);
+        }
+
+        String getValue(XmlNode node, String subNode, String defVal = "-")
+        {
+            XmlNode sub = node?[subNode];
+            if(sub == null) {
+                return defVal;
+            }
+            return sub.InnerText;
+        }
+
+        /// <summary>
+        /// convert the German special chars to HTML-encoding
+        /// </summary>
+        /// <param name="orgStr"></param>
+        /// <returns></returns>
+        String htmlEncode(String orgStr)
+        {
+            StringBuilder sb = new StringBuilder();
+            Dictionary<char, String> transl = new Dictionary<char, string> {
+                { 'Ä', "&#196;"}, {'ä', "&#228;"}, {'Ö', "&#214;"}, {'ö', "&#246;"},
+                { 'Ü', "&#220;"}, {'ü', "&#252;"}, {'ß', "&#223;"}};
+            foreach(char c in orgStr) {
+                String outstr;
+                if(transl.TryGetValue(c, out outstr)) {
+                    sb.Append(outstr);
+                } else {
+                    sb.Append(c);
+                }
+            }
+            return sb.ToString();
+        }
+
+        static void AddNode(XmlNode parent, String name, String inner = "")
+        {
+
+            // get the whitespace text from this node, or if not found from parent node
+            XmlNode exwhsp = parent["#whitespace"];
+            String whiteInner = "\r\n  ";
+            if(exwhsp != null) {
+                whiteInner = exwhsp.InnerText;
+            } else if(parent.ParentNode != null && parent.ParentNode["#whitespace"] != null) {
+                whiteInner = parent.ParentNode["#whitespace"].InnerText + "  ";
+            }
+
+            // add the whitespace node, the actual node and the trailing whitespace-node
+            // if the last child is a whitespace, replace its text to whiteInner, otherwise add a node
+            if(parent.LastChild != null && parent.LastChild.Name == "#whitespace") {
+                parent.LastChild.InnerText = whiteInner;
+            } else {
+                _SimpleAddNode(parent, "#whitespace", whiteInner);
+            }
+            _SimpleAddNode(parent, name, inner);
+            _SimpleAddNode(parent, "#whitespace", "\r\n");
+
+            void _SimpleAddNode(XmlNode _parent, String _name, String _inner)
+            {
+                XmlNode newNode;
+                if(_name == "#whitespace") {
+                    newNode = _parent.OwnerDocument.CreateWhitespace(_inner);
+                } else {
+                    newNode = _parent.OwnerDocument.CreateElement(_name);
+                }
+                if(_inner != "") {
+                    newNode.InnerText = _inner;
+                }
+                parent.AppendChild(newNode);
+            }
+
+        }
+
+        #endregion iTrain access
+    }
+}
